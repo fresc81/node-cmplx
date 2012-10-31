90c68be73f3e0a3c7c2e954918ea3485ac30e515
Paul Bottin <paulbottin+git@gmail.com>
2012-10-31 11:05:23 +0100

    added JSDoc comments

--- test.js
+++ test.js
@@ -17,19 +17,19 @@ var cmplx = require('./lib/cmplx')
 ;
 
 console.log('z1_A: %s', z1_A);
-console.log('z1_P: %s', z1_P);
+console.log('z1_P: %s -> %s', z1_P, z1_P.toArith());
 console.log('z1_AC: %s', z1_AC);
-console.log('z1_PC: %s', z1_PC);
+console.log('z1_PC: %s -> %s', z1_PC, z1_PC.toArith());
 
 console.log();
 
 console.log('z2_P: %s', z2_P);
-console.log('z2_A: %s', z2_A);
+console.log('z2_A: %s -> %s', z2_A, z2_A.toPolar());
 console.log('z2_PC: %s', z2_PC);
-console.log('z2_AC: %s', z2_AC);
+console.log('z2_AC: %s -> %s', z2_AC, z2_AC.toPolar());
 
 console.log();
 
-console.log('z3_AP: %s', z3_AP);
-console.log('z3_PA: %s', z3_PA);
+console.log('z3_AP: %s -> %s', z3_AP, z3_AP.toPolar());
+console.log('z3_PA: %s -> %s', z3_PA, z3_PA.toArith());
 
